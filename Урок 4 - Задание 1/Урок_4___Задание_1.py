da205b4ef81c00ba4c80fb005e85847fa94c45da
IIIu6ko <iiiu6ko@gmail.com>
2024-03-18 19:29:17 +0300

    Дорабатывает задание 1 из 4 урока

--- Урок 4 - Задание 1/Урок_4___Задание_1.py
+++ Урок 4 - Задание 1/Урок_4___Задание_1.py
@@ -1,4 +1,15 @@
 print('Введите стороны прямоугольника, чтобы узнать его площадь и периметр:')
-a, b = map(int, input().split())
+a, b = input().split()
+
+if a.isdigit():
+    a = int(a)
+else:
+    a = float(a)
+    
+if b.isdigit():
+    b = int(b)
+else:
+    b = float(b)
+
 print('Площадь прямоугольника = ', a * b)
 print('Периметр прямоугольника = ', 2 * (a + b))
